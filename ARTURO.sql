c16a19ce12f55d27089ba2b4b360fa7627cfefc7
Pascual <pascual.munoz@pccomponentes.com>
2016-12-15 11:48:03 +0100

    mejorada por programación y arturijo

--- ARTURO.sql
+++ ARTURO.sql
@@ -1,35 +1,17 @@
+set @dia = '2016-12-14';
 
-
-select r.created as dt_creacion_caso,
-a.dt as dt_generada_asistente,
-a.provincia,  
-timediff(a.dt, r.created) as Lapso ,
-t.name as tipo,
-r.returnId as Numero_Caso, a.*
+select r.typeId, r.returnId, r.created, r.reference,r.firstReference, r.productId,
+a.dt as fechaRecogida,a.id_recogida,a.id_caso,a.cp,a.transporte, a.nRecogida, a.user,a.recepcionada,
+ac.dt,ac.email_cliente, ac.pedido, ac.aceptado,ac.transporte,ac.recogida
 from pccom_returns r
 left join asistente_recogidas a on r.returnId = a.id_caso
-left join pccom_returns_types t on t.typeId=r.typeId
-where
-date(r.created) = SUBDATE(current_date, 1)
-order by r.created;
-
-
-
-
-select r.created as dt_creacion_caso,
-a.dt as dt_generada_asistente,
-a.provincia,  
-timediff(a.dt, r.created) as Lapso ,
-t.name as tipo,
-r.returnId as Numero_Caso, a.*
-from pccom_returns r
-left join asistente_recogidas a on r.returnId = a.id_caso
-left join pccom_returns_types t on t.typeId=r.typeId
-where
-date(r.created) = SUBDATE(current_date, 1)
-and a.provincia in ( 'Ceuta' , 'Melilla')
-order by r.created;
-
-
-
-
+left join asistente_casos ac on r.returnId = ac.id_caso
+left join pccom_returns_address d on r.returnId = d.returnId
+left join clientes_tarifas c on r.clientId = c.id_cliente
+where date(r.created) = @dia
+and r.typeId=1
+and r.subtypeId=0
+and r.shop=0
+and d.state not in('%Ceuta%','%Melilla%')
+and c.id_cliente is null
+and a.nRecogida is null; 
