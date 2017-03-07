4474ae373456d8aa2a2b3c4c9bd463aeb897662e
Pascual <pascual.munoz@pccomponentes.com>
2017-03-07 08:40:11 +0100

    reorganizadas consultas por carpetas.

--- ALMACEN/EVENTOS/EV_ALM_ALBARANAR_PEDIDO.sql
+++ ALMACEN/EVENTOS/EV_ALM_ALBARANAR_PEDIDO.sql
@@ -1,8 +1,6 @@
-
-
-/*campos comunes ID , EVENTO , DT , FECHA , HORA , ID_USER */
-
-select
+select 
+	w.orderNumber				  as EV_ALMACEN_ALBARANAR_PEDIDO_pedido,
+	count(*),					  as lineas_en_pedido
 	w.user						  as id_user, 
     'EV_ALMACEN_ALBARANAR_PEDIDO' as `EVENTO`, 
 	w.dateWaybilled				  as dt, 
@@ -14,5 +12,6 @@ select
 FROM 
 	componentes.logistic_to_waybill w
 	inner join componentes.pccom_logistic_orders_stickers_articles a ON w.orderNumber = a.orderNumber AND CAST(w.stickerSuffix AS UNSIGNED) = a.stickerSuffix
+group by EV_ALMACEN_ALBARANAR_PEDIDO_pedido
 
 limit 1000;
