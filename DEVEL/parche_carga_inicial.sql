72281cabdd6f1e55405fb21459e169cfd589d895
Pascual Muñoz <pascual.munoz@pccomponentes.com>
2017-03-31 23:03:57 +0200

    para la carga inicial del doofinder.

---
+++ DEVEL/parche_carga_inicial.sql
@@ -0,0 +1,20 @@
+SELECT
+    (@cnt := @cnt + 1) AS rowNumber,
+    a.id_articulo
+FROM articulos a  
+  CROSS JOIN (SELECT @cnt := 0) AS dummy
+WHERE a.activo = 's' 
+and a.id_articulo not in (2, 10003, 10004, 10005, 10006, 11000, 19000, 20000, 21004, 21010, 21017, 23001, 31044, 31045, 31046, 54958, 54959,
+			55996, 55998, 63258, 65450, 63315, 63313, 59051) 
+ORDER BY a.id_articulo desc ;
+
+
+
+select min(a.id_articulo) , max(a.id_articulo)
+from articulos a 	
+where a.activo = 's'
+order by a.id_articulo
+;
+select (@cnt = @cnt+1),  a.id_articulo
+from articulos a 
+order by a.id_articulo; 
